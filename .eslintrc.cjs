b9dd5a5c1cc3306ff0477fcec1e49386e52e9849
rovxxmjxx <miju3545@gmail.com>
2022-12-30 16:14:48 +0900

    chore: delete unnessary files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,16 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    jsx: true,
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
